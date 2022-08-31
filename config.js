db115ec5ffe85bd2982532a54fa4e3ba9c319637
madfurytr <66790237+madfurytr@users.noreply.github.com>
2022-08-31 15:53:02 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    token: "", 
+    prefix: "e!",
+    botdavet: "(zorunlu)",
+    desteksunucusu: "(zorunlu)",
+    website: "(zorunlu)",
+    topgg: "(zorunlu)",
+    politika: "(zorunlu)",
+    sunucuid: "(zorunlu)",
+    destekçi: "(zorunlu)"
+}
+
